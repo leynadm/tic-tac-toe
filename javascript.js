477abb8709e35bd520bf904fe279465ee4db2ca0
Leynad <matei_daniel@outlook.com>
2022-10-08 23:18:43 +0200

    Add initial object literal module logic - had an eureka moment here :)

--- javascript.js
+++ javascript.js
@@ -1,28 +1,62 @@
-(function (){
-    var gameBoard = {
-        gameState: ["X","0","X","0","X","0","X","0","X"],
-        init: function(){
-            this.cacheDom();
-        },
+//(function(){
+var gameBoard = {
+    gameState: [null,null,null,null,null,null,null,null,null],
+    init: function(){
+        this.cacheDom();
+        this.bindEvents();
+        this.render();
+    },
+    cacheDom: function(){
 
-        cacheDom: function(){
-            var boardSquares = document.querySelectorAll('[data-tablesquare]')
-            var playerOne = document.querySelector('.player-one');
-            console.log(playerOne);
-            var playerTwo = document.querySelector('.player-two');
+        let boardSquares = document.querySelectorAll('[data-tablesquare]')
+        let playerOneInput = document.querySelector('.player-one');
+        let playerTwoInput = document.querySelector('.player-two');
+
+        return {boardSquares, playerOneInput, playerTwoInput}
+    },
+
+    bindEvents: function(){
+        this.cacheDom().boardSquares.forEach(element => {
+            element.addEventListener('click',this.addPlayerInput);
+        });
+
+        this.cacheDom().playerOneInput.addEventListener('keyup',this.getPlayerName);
+
+    },
+
+    render: function(){
+    
+    },
+
+    addPlayerInput: function(){
+        let firstItemAdded;
+        let squareSelected = this.dataset.tablesquare;
+
+        if(firstItemAdded==null){
+            firstItemAdded = "X";
+        } else if (firstItemAdded != null && firstItemAdded == "O"){
+            firstItemAdded = "O";
+        } else {
+            firstItemAdded = "X";
         }
 
-        
+
+        gameBoard.gameState.splice(squareSelected,0,firstItemAdded);
+        console.log(gameBoard.gameState);    
+               
+    },
+
+    getPlayerName: function(){
+        console.log(this.value);
+    },
+
+    cleanGameBoard: function(){
 
     }
 
-    gameBoard.init();
-
-})();
-
-
-// Factory function to create the player instances
-const player = (playerName) => {
-
 }
+
+gameBoard.init();
+
+//})();
 
