eeacb5dbd9fcd531cf3b2110f982c40b0fa5cbdd
Leynad <matei_daniel@outlook.com>
2022-10-10 23:03:07 +0200

    Add the ability for player to submit their input

--- javascript.js
+++ javascript.js
@@ -1,13 +1,12 @@
 //(function(){
 var gameBoard = {
     gameState: [null,null,null,null,null,null,null,null,null],
+    playerOneTurn:true,
     init: function(){
         this.cacheDom();
         this.bindEvents();
-        this.render();
     },
     cacheDom: function(){
-
         let boardSquares = document.querySelectorAll('[data-tablesquare]')
         let playerOneInput = document.querySelector('.player-one');
         let playerTwoInput = document.querySelector('.player-two');
@@ -19,31 +18,35 @@ var gameBoard = {
         this.cacheDom().boardSquares.forEach(element => {
             element.addEventListener('click',this.addPlayerInput);
         });
-
         this.cacheDom().playerOneInput.addEventListener('keyup',this.getPlayerName);
-
     },
 
     render: function(){
-    
+
     },
 
     addPlayerInput: function(){
-        let firstItemAdded;
+        let XorO;
         let squareSelected = this.dataset.tablesquare;
+        let squareSelectedValue = this.textContent;
 
-        if(firstItemAdded==null){
-            firstItemAdded = "X";
-        } else if (firstItemAdded != null && firstItemAdded == "O"){
-            firstItemAdded = "O";
-        } else {
-            firstItemAdded = "X";
+        if(squareSelectedValue!=''){
+            alert('This cell has already been selected!');
+            return;
         }
 
+        if(gameBoard.playerOneTurn){
+            XorO = "X";
+            gameBoard.playerOneTurn = false;
+        } else if (gameBoard.playerOneTurn==false){
+            XorO = "O";           
+            gameBoard.playerOneTurn = true;
+        }
 
-        gameBoard.gameState.splice(squareSelected,0,firstItemAdded);
-        console.log(gameBoard.gameState);    
-               
+        gameBoard.gameState[squareSelected] = XorO;        
+        console.log(gameBoard.gameState[squareSelected])
+        gameBoard.cacheDom().boardSquares[squareSelected].textContent = XorO;
+
     },
 
     getPlayerName: function(){
@@ -51,6 +54,58 @@ var gameBoard = {
     },
 
     cleanGameBoard: function(){
+
+    },
+
+    checkCurrentState: function(currentPlayer){
+        let square_00 = gameBoard.gameState[0];
+        let square_01 = gameBoard.gameState[1];
+        let square_02 = gameBoard.gameState[2];
+        let square_03 = gameBoard.gameState[3];
+        let square_04 = gameBoard.gameState[4];
+        let square_05 = gameBoard.gameState[5];
+        let square_06 = gameBoard.gameState[6];
+        let square_07 = gameBoard.gameState[7];
+        let square_08 = gameBoard.gameState[8];
+        let square_09 = gameBoard.gameState[9];
+        
+        let winner_series_01 = square_00+square_01+square_02;
+        let winner_series_02 = square_00+square_03+square_06;
+        let winner_series_03 = square_06+square_07+square_08;
+        let winner_series_04 = square_02+square_05+square_08;
+        let winner_series_05 = square_01+square_04+square_07;
+        let winner_series_06 = square_00+square_04+square_05;
+        let winner_series_07 = square_02+square_04+square_06;
+        
+        currentPlayer = "XXX";
+
+        switch(currentPlayer){
+            case winner_series_01:
+                console.log("Works");
+                break;
+            case winner_series_02:
+                console.log("Works");
+                break;
+            case winner_series_03:
+                console.log("Works");
+                break;
+            case winner_series_04:
+                console.log("Works");
+                break;
+            case winner_series_05:
+                console.log("Works");
+                break;
+            case winner_series_06:
+                console.log("Works");
+                break;
+            case winner_series_07:
+                console.log("Works");
+                break;
+        }
+
+    },
+
+    declareWinner: function() {
 
     }
 
